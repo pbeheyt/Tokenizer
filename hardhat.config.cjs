00ecefeef47b6a24094cbfe62ef19c251c0629f0
pbeheyt <pierre.beheyt.bba@edhec.com>
2025-08-30 14:05:51 +0200

    add deployment script and config for BNB Testnet

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    bscTestnet: {
+      url: process.env.BSC_TESTNET_RPC_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
