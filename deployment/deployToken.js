c8fd7094b36a843618b195bcfcafb03962e65b32
pbeheyt <pierre.beheyt.bba@edhec.com>
2025-08-30 16:22:55 +0200

    reorganise folder

---
+++ deployment/deployToken.js
@@ -0,0 +1,22 @@
+import pkg from "hardhat";
+const { ethers } = pkg;
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contract with the account:", deployer.address);
+
+  const tokenFactory = await ethers.getContractFactory("Token42");
+  const token = await tokenFactory.deploy(deployer.address);
+
+  await token.waitForDeployment();
+
+  console.log(`Token deployed to: ${token.target}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -5,6 +5,13 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  paths: {
+    sources: "./code",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+    scripts: "./deployment",
+  },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY || "",
   },
