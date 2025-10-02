c23f55983aaca03cff32180b77257a67cb7786b4
Pierre BEHEYT <pierre.beheyt.bba@edhec.com>
2025-10-02 18:15:15 +0200

    Add tests for token transfer functionality

--- test/token.test.js
+++ test/token.test.js
@@ -34,4 +34,34 @@ describe("Token42", function () {
       expect(ownerBalance).to.equal(expectedSupply);
     });
   });
+
+  describe("Transactions", function () {
+    it("Should transfer tokens between accounts", async function () {
+      // Transfer 50 tokens from owner to addr1
+      await token.connect(owner).transfer(addr1.address, ethers.parseUnits("50", 18));
+      const addr1Balance = await token.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(ethers.parseUnits("50", 18));
+
+      // Check owner's balance reduction
+      const initialOwnerBalance = ethers.parseUnits("1000000", 18);
+      const expectedOwnerBalance = initialOwnerBalance - ethers.parseUnits("50", 18);
+      const ownerBalance = await token.balanceOf(owner.address);
+      expect(ownerBalance).to.equal(expectedOwnerBalance);
+    });
+
+    it("Should fail if sender doesn't have enough tokens", async function () {
+      const initialOwnerBalance = await token.balanceOf(owner.address);
+
+      // Try to send 1 token from addr1 (0 tokens) to owner.
+      // `require` will evaluate false and revert the transaction.
+      await expect(
+        token.connect(addr1).transfer(owner.address, ethers.parseUnits("1", 18))
+      ).to.be.revertedWithCustomError(token, "ERC20InsufficientBalance");
+
+      // Owner's balance should not have changed.
+      expect(await token.balanceOf(owner.address)).to.equal(
+        initialOwnerBalance
+      );
+    });
+  });
 });
